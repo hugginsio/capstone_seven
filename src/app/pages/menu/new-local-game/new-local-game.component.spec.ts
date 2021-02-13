f8c8c18a660bc31224d39544721ad3368e20c7de
Kyle Huggins <5430818+kjhx@users.noreply.github.com>
2021-02-13 16:45:07 -0600

    new-local-game: test setting flip methods

--- src/app/pages/menu/new-local-game/new-local-game.component.spec.ts
+++ src/app/pages/menu/new-local-game/new-local-game.component.spec.ts
@@ -10,7 +10,7 @@ describe('NewLocalGameComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [ NewLocalGameComponent ]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +21,21 @@ describe('NewLocalGameComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('changeGameMode should flip game modes', () => {
+    // Assumes vs AI is the default mode.
+    component.changeGameMode();
+    expect(component.gameModeString).toEqual(component.pvp);
+    component.changeGameMode();
+    expect(component.gameModeString).toEqual(component.pva);
+  });
+
+  it('changeAiDifficulty should flip AI difficulty', () => {
+    // Assumes AI is set to easy by default.
+    component.changeAiDifficulty();
+    expect(component.aiDifficultyString).toEqual(component.aiMedium);
+    component.changeAiDifficulty();
+    expect(component.aiDifficultyString).toEqual(component.aiEasy);
   });
 });
