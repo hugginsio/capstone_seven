a6c9f45c0c4be8184aa615e642f325d2a419557d
Kyle Huggins <5430818+kjhx@users.noreply.github.com>
2021-02-16 21:12:19 -0600

    ai service: remove difficulty constructor param

--- src/app/pages/game/services/ai/ai.service.ts
+++ src/app/pages/game/services/ai/ai.service.ts
@@ -1,10 +1,12 @@
-import { Injectable } from '@angular/core';
+import { Injectable, InjectionToken } from '@angular/core';
 
 import { MonteCarlo } from '../..//classes/ai/ai.class.MonteCarlo';
 import { State } from '../../classes/ai/ai.class.State';
 import { GameBoard } from '../../classes/gamecore/game.class.GameBoard';
 import { Player } from '../../classes/gamecore/game.class.Player';
 import { CoreLogic } from '../../util/core-logic.util';
+
+const DIFF_TOKEN = new InjectionToken<string>('MyToken');
 
 @Injectable({
   providedIn: 'root'
@@ -14,15 +16,13 @@ export class AiService {
   mcts: MonteCarlo;
   explorationParameter:number;
   currentState:State;
-  difficulty:string;
+  // difficulty:string; TODO
 
-  constructor(gameBoard:GameBoard, player1:Player,player2:Player, difficulty:string) {
+  constructor(gameBoard:GameBoard, player1:Player,player2:Player) {
     this.explorationParameter = 1.41;
     this.mcts = new MonteCarlo(gameBoard, this.explorationParameter);
 
     this.currentState = CoreLogic.getStartingState(player1, player2,gameBoard,1);
-    
-    this.difficulty = difficulty;
   }
 
   getMove(previousMove:string):string{
