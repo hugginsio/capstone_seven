9fb008f20c9e896c776f7c02d226dda93847bf25
hmargy <halliemargy@sbcglobal.net>
2021-02-27 14:25:38 -0600

    networking service change

--- src/app/networking.service.ts
+++ src/app/networking.service.ts
@@ -9,8 +9,6 @@ export class NetworkingService {
 
   constructor() { }
 
-<<<<<<< Updated upstream
-=======
   const EventEmitter = require('events');
   const myEmitter = new this.EventEmitter();
 
@@ -19,21 +17,5 @@ export class NetworkingService {
   });
 
   myEmitter.emit('event', 1, 2);
-  // has the match already been made ?
-
-  // if host, send board to p2
-  // else, receive board and send as seed to game manager
-
-  // is host p1 always?
-  // branches must be sent in the order they were placed
-
-  // send players moves at endTurn
-  // endTurn function can send us the move in string form
-  // receive string and sent to game manager
-  // *** game manager must put in a function to separate out the string
-  // *** also maybe a function to set it as a string ?
-
-  // at end of game, option to exit or to have a new game
->>>>>>> Stashed changes
   
 }
