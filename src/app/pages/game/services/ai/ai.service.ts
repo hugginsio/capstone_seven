51d0ef93e479408fa7380ae2c6777a76212c381b
jdhat <61174487+jdhat@users.noreply.github.com>
2021-03-01 09:59:18 -0600

    Final random ai and dev tools in electron

--- src/app/pages/game/services/ai/ai.service.ts
+++ src/app/pages/game/services/ai/ai.service.ts
@@ -69,9 +69,14 @@ export class AiService {
     const moves = CoreLogic.getLegalMoves(this.currentState);
 
 
+    
     const resultIndex = this.weightMoves(moves);
-    const result = moves[resultIndex];
+    let result = moves[resultIndex];
 
+
+    if(result === undefined){
+      result = ';;';
+    }
 
     const newState = CoreLogic.nextState(this.currentState, result);
     this.currentState = newState;
@@ -105,7 +110,11 @@ export class AiService {
    
     const resultIndex = this.weightMoves(moves);
 
-    const result = moves[resultIndex];
+    let result = moves[resultIndex];
+
+    if(result === undefined){
+      result = ';;';
+    }
 
     this.currentState = CoreLogic.nextState(this.currentState, result);
 
@@ -114,15 +123,145 @@ export class AiService {
 
   weightMoves(moves:string[]):number{
     const weights = Array<string>();
+    const currentPlayer = this.currentState.currentPlayer === 1 ? this.currentState.player1 : this.currentState.player2;
 
     for(let i = 0; i<moves.length;i++){
       const moveObj = CoreLogic.stringToMove(moves[i]);
+      const red = currentPlayer.redResources;
+      const blue = currentPlayer.blueResources;
+      const green = currentPlayer.greenResources;
+      const yellow = currentPlayer.yellowResources;
+
+      let numRedTraded = 0;
+      let numBlueTraded = 0;
+      let numGreenTraded = 0;
+      let numYellowTraded = 0;
+
       if(moveObj.tradedIn.length > 0){
+        for(const resource of moveObj.tradedIn){
+          switch(resource){
+            case 'R':
+              numRedTraded++;
+              break;
+            case 'B':
+              numBlueTraded++;
+              break;
+            case 'G':
+              numGreenTraded++;
+              break;
+            case 'Y':
+              numYellowTraded++;
+              break;
+          }
+        }
+
+        if(moveObj.received === 'R'){
+          if((blue - numBlueTraded) >=1){
+            weights.push(moves[i]);
+            if(moveObj.branchesPlaced.length > 0){
+              weights.push(moves[i]);
+            }
+          }
+        }
+        if(moveObj.received === 'B'){
+          if((red - numRedTraded) >=1){
+            weights.push(moves[i]);
+            if(moveObj.branchesPlaced.length > 0){
+              weights.push(moves[i]);
+            }
+          }
+        }
+        if(moveObj.received === 'G'){
+          if((green - numGreenTraded) >=2){
+            weights.push(moves[i]);
+            if(moveObj.nodesPlaced.length > 0){
+              weights.push(moves[i]);
+            }
+          }
+        }
+        if(moveObj.received === 'Y'){
+          if((yellow - numYellowTraded) >=2){
+            weights.push(moves[i]);
+            if(moveObj.nodesPlaced.length > 0){
+              weights.push(moves[i]);
+            }
+          }
+        }
+      }
+      else{
         weights.push(moves[i]);
       }
+
+
+      const cornerNodes = [2,0,1,5,6,12,11,17,18,21,22,23];
       if(moveObj.nodesPlaced.length > 0){
-        weights.push(moves[i]);
+        
+        for(const nodeIndex of moveObj.nodesPlaced){
+         
+          if(this.currentState.gameBoard.nodes[nodeIndex].getTopRightTile() !== -1 && this.currentState.gameBoard.tiles[this.currentState.gameBoard.nodes[nodeIndex].getTopRightTile()].getMaxNodes() === 3){
+            weights.push(moves[i]);
+            weights.push(moves[i]);  
+            weights.push(moves[i]);        
+          }
+          else if(this.currentState.gameBoard.nodes[nodeIndex].getTopRightTile() !== -1 && this.currentState.gameBoard.tiles[this.currentState.gameBoard.nodes[nodeIndex].getTopRightTile()].getMaxNodes() === 2){ 
+            weights.push(moves[i]);
+            weights.push(moves[i]);
+          }
+          else if(this.currentState.gameBoard.nodes[nodeIndex].getTopRightTile() !== -1 && this.currentState.gameBoard.tiles[this.currentState.gameBoard.nodes[nodeIndex].getTopRightTile()].getMaxNodes() === 1){ 
+            weights.push(moves[i]);
+          }
+
+          if(this.currentState.gameBoard.nodes[nodeIndex].getBottomRightTile() !== -1 && this.currentState.gameBoard.tiles[this.currentState.gameBoard.nodes[nodeIndex].getBottomRightTile()].getMaxNodes() === 3){
+            weights.push(moves[i]);
+            weights.push(moves[i]);  
+            weights.push(moves[i]);        
+          }
+          else if(this.currentState.gameBoard.nodes[nodeIndex].getBottomRightTile() !== -1 && this.currentState.gameBoard.tiles[this.currentState.gameBoard.nodes[nodeIndex].getBottomRightTile()].getMaxNodes() === 2){ 
+            weights.push(moves[i]);
+            weights.push(moves[i]);
+          }
+          else if(this.currentState.gameBoard.nodes[nodeIndex].getBottomRightTile() !== -1 && this.currentState.gameBoard.tiles[this.currentState.gameBoard.nodes[nodeIndex].getBottomRightTile()].getMaxNodes() === 1){ 
+            weights.push(moves[i]);
+          }
+
+          if(this.currentState.gameBoard.nodes[nodeIndex].getBottomLeftTile() !== -1 && this.currentState.gameBoard.tiles[this.currentState.gameBoard.nodes[nodeIndex].getBottomLeftTile()].getMaxNodes() === 3){
+            weights.push(moves[i]);
+            weights.push(moves[i]);  
+            weights.push(moves[i]);        
+          }
+          else if(this.currentState.gameBoard.nodes[nodeIndex].getBottomLeftTile() !== -1 && this.currentState.gameBoard.tiles[this.currentState.gameBoard.nodes[nodeIndex].getBottomLeftTile()].getMaxNodes() === 2){ 
+            weights.push(moves[i]);
+            weights.push(moves[i]);
+          }
+          else if(this.currentState.gameBoard.nodes[nodeIndex].getBottomLeftTile() !== -1 && this.currentState.gameBoard.tiles[this.currentState.gameBoard.nodes[nodeIndex].getBottomLeftTile()].getMaxNodes() === 1){ 
+            weights.push(moves[i]);
+          }
+
+          if(this.currentState.gameBoard.nodes[nodeIndex].getTopLeftTile() !== -1 && this.currentState.gameBoard.tiles[this.currentState.gameBoard.nodes[nodeIndex].getTopLeftTile()].getMaxNodes() === 3){
+            weights.push(moves[i]);
+            weights.push(moves[i]);  
+            weights.push(moves[i]);        
+          }
+          else if(this.currentState.gameBoard.nodes[nodeIndex].getTopLeftTile() !== -1 && this.currentState.gameBoard.tiles[this.currentState.gameBoard.nodes[nodeIndex].getTopLeftTile()].getMaxNodes() === 2){ 
+            weights.push(moves[i]);
+            weights.push(moves[i]);
+          }
+          else if(this.currentState.gameBoard.nodes[nodeIndex].getTopLeftTile() !== -1 && this.currentState.gameBoard.tiles[this.currentState.gameBoard.nodes[nodeIndex].getTopLeftTile()].getMaxNodes() === 1){ 
+            weights.push(moves[i]);
+          }
+          if(cornerNodes.includes(nodeIndex)){
+            while(weights.includes(moves[i])){
+              weights.splice(weights.indexOf(moves[i]),1);
+            }
+          }
+          weights.push(moves[i]);
+        }
+        
+          
+        
       }
+
+
       if(moveObj.branchesPlaced.length > 0){
         weights.push(moves[i]);
       }
@@ -130,16 +269,20 @@ export class AiService {
       if(moveObj.nodesPlaced.includes(8)){
         weights.push(moves[i]);
         weights.push(moves[i]);
+        weights.push(moves[i]);
       }
       if(moveObj.nodesPlaced.includes(9)){
+        weights.push(moves[i]);
         weights.push(moves[i]);
         weights.push(moves[i]);
       }
       if(moveObj.nodesPlaced.includes(14)){
         weights.push(moves[i]);
         weights.push(moves[i]);
+        weights.push(moves[i]);
       }
       if(moveObj.nodesPlaced.includes(15)){
+        weights.push(moves[i]);
         weights.push(moves[i]);
         weights.push(moves[i]);
       }
@@ -180,14 +323,29 @@ export class AiService {
           clonedState.tilesBeingChecked = [];
           if (CoreLogic.checkForCaptures(clonedState,affectedPlayer, i) === true) {
             weights.push(moves[i]);
+            weights.push(moves[i]);
           }
         }
       }
+
+      if(this.currentState.inInitialMoves){
+        for(const nodeIndex of moveObj.nodesPlaced){
+          if(cornerNodes.includes(nodeIndex)){
+            while(weights.includes(moves[i])){
+              weights.splice(weights.indexOf(moves[i]),1);
+            }
+          }
+        }
+      }
+      
+
 
       if(moveObj.tradedIn.length === 0 && moveObj.nodesPlaced.length === 0 && moveObj.branchesPlaced.length === 0){
         weights.push(moves[i]);
       }
     }
+
+
     console.log(weights.length, weights);
     return moves.indexOf(weights[Math.floor(Math.random()*weights.length)]);
   }
