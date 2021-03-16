53053945f2d57c481a6888d191b8c189e9802b47
Kyle Huggins <5430818+kjhx@users.noreply.github.com>
2021-03-15 22:23:15 -0500

    shared: adjust imports, exports

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { RoutingErrorComponent } from './components/';
 import { FormsModule } from '@angular/forms';
+import { SnackbarComponent } from './components/';
 
 @NgModule({
-  declarations: [RoutingErrorComponent],
+  declarations: [RoutingErrorComponent, SnackbarComponent],
   imports: [CommonModule, FormsModule],
-  exports: [FormsModule]
+  exports: [FormsModule, SnackbarComponent]
 })
 export class SharedModule {}
