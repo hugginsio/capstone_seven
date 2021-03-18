ac0976e77141b579340fedebc2a3a41717046577
hmargy <halliemargy@sbcglobal.net>
2021-03-17 21:05:06 -0500

    start to help slideshow

--- src/app/pages/menu/help/menu-help.component.ts
+++ src/app/pages/menu/help/menu-help.component.ts
@@ -3,13 +3,36 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-menu-help',
   templateUrl: './menu-help.component.html',
-  styleUrls: ['../menu-common.scss']
+  styleUrls: ['../menu-common.scss', './help.scss'],
 })
 export class MenuHelpComponent implements OnInit {
 
-  constructor() { }
+  //public slideIndex: number;
+
+  constructor() { 
+    //this.slideIndex = 1;
+  }
 
   ngOnInit(): void {
   }
 
+  //https://www.w3schools.com/howto/howto_js_slideshow.asp
+  /*plusSlides(n: number):void {
+    showSlides(slideIndex += n);
+  }
+
+  currentSlide (n: number):void {
+    showSlides(slideIndex = n);
+  }
+
+  showSlides(n: number):void {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+    if(n>slides.length) { this.slideIndex = 1 }
+    if (n < 1) { this.slideIndex = slides.length }
+    for (i = 0; i < slides.length, i++)
+    {
+      slides[i].isVisible = false;
+    }
+  }*/
 }
