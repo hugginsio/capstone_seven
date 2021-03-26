df5f56085364af3871d41186b1e6a73d38191b9c
hmargy <halliemargy@sbcglobal.net>
2021-03-25 22:29:51 -0500

    skeleton of functionality & test button code

--- src/app/pages/game/guided-tutorial/guided-tutorial.component.ts
+++ src/app/pages/game/guided-tutorial/guided-tutorial.component.ts
@@ -8,6 +8,7 @@ import { ClickEvent, GuidedTutorialInterface } from '../interfaces/game.interfac
 import { ManagerService } from '../services/gamecore/manager.service';
 import { TradingModel } from '../models/trading.model';
 import { SnackbarService } from '../../../shared/components/snackbar/services/snackbar.service';
+import { GuidedTutorialService } from '../services/guided-tutorial/guided-tutorial.service';
 
 @Component({
   selector: 'app-guided-tutorial',
@@ -16,97 +17,34 @@ import { SnackbarService } from '../../../shared/components/snackbar/services/sn
 })
 export class GuidedTutorialComponent implements OnInit {
 
-  public readonly GT = new Subject<GuidedTutorialInterface>();
+  //public readonly GT = new Subject<GuidedTutorialInterface>();
 
   constructor(
     @Inject(DOCUMENT) private document: Document,
     public readonly gameManager: ManagerService,
     private readonly storageService: LocalStorageService,
-    private readonly snackbarService: SnackbarService
+    private readonly snackbarService: SnackbarService,
+    private readonly GTService: GuidedTutorialService
   ){ 
     this.storageService.setContext('game');
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
 
-    if(this.storageService.fetch('guided-tutorial') === 'true' 
-      && this.storageService.fetch('mode') === 'pva'
-      && this.storageService.fetch('ai-difficulty') === 'easy') {
-
-      // Subscribe to own communications link
-      //this.GT.subscribe(message => {
-        //const status = message.message;
-
-        // human is player1
-        if(this.storageService.fetch('firstplayer') === 'one') {
-          // message 1, start tutorial
-          this.snackbarService.add({ message: 'Welcome in to the mines!' });
-          this.snackbarService.add({ message: 'Let’s walk you through a few steps to get y’all on the right foot with this here duel in the mines.' });
-          // click next
-
-          // message 2
-          //this.snackbarService.add({ message: 'Learn some mining lingo before we get going: Pickaxes and Drills are what we call "Mining Markers"' });
-          //this.snackbarService.add({ message: 'Tracks get you from place to place, and these here squares here are mining sites.' });
-          //this.snackbarService.add({ message: 'These sites have gems in ‘em that hold the type and number of gem you can mine from there.' });
-          // click next
-
-          // message 3
-          //this.snackbarService.add({ message: 'Start the competition by puttin\' down a pickaxe and a connectin\' track anywhere in the mine'});
-          this.snackbarService.add({ message: 'Click the indicated marker and track to make your move' });
-          // moves after pieces have been clicked
-
-          // message 4
-         // this.snackbarService.add({ message: 'You get gems based on the corners of the minin\' sites you have your pickaxe touchin\'' });
-          //this.snackbarService.add({ message: 'You gotta have gems in the future to get more pickaxes and tracks down the road' });
-          //this.snackbarService.add({ message: 'Don\'t like where you clicked? Click the "Undo" button' });
-          //this.snackbarService.add({ message: 'Settled on your move? Click the "End Turn" button to keep the game movin\'' });
-          // moves when "End Turn" is clicked
-
-
-        }
-        //this.snackbarService.add({ message: 'You cannot trade right now.' });
-
-
-      // Check which player sent the message before we run player-centric commands
-      /*if (this.gameManager.getCurrentPlayer() === message.player) {
-        if (status === CommCode.IS_TRADING) {
-          const currentPlayer = this.gameManager.getCurrentPlayer();
-          if (currentPlayer.numNodesPlaced < 2 || currentPlayer.ownedBranches.length < 2) {
-            this.snackbarService.add({ message: 'You cannot trade right now.' });
-          /*} else {
-            this.isTrading = true;
-            this.toggleTrade();
-          }
-        } else if (status === CommCode.END_TURN) {
-          const currentPlayer = this.gameManager.getCurrentPlayer();
-          if ((currentPlayer.numNodesPlaced < 2 || currentPlayer.ownedBranches.length < 2) &&
-            (currentPlayer.redResources !== 0 || currentPlayer.greenResources !== 0 ||
-              currentPlayer.blueResources !== 0 || currentPlayer.yellowResources !== 0)) {
-            this.snackbarService.add({ message: 'You must place a node and a branch.' });
-          } else {
-            this.gameManager.endTurn(this.gameManager.getCurrentPlayer());
-          }
-        } else if (status === CommCode.END_GAME) {
-          this.gameOverText = `${this.gameManager.getCurrentPlayerEnum()} Victorious!`;
-          this.winningPlayer = this.gameManager.getCurrentPlayer();
-          this.gameOver = true;
-        } else if (status === CommCode.UNDO) {
-          const gamePiece = this.gameManager.stack.pop();
-          if (gamePiece) {
-            this.gameManager.undoPlacement(gamePiece[0] as string, gamePiece[1] as number, this.gameManager.getCurrentPlayer());
-          } else {
-            this.snackbarService.add({ message: 'No moves to undo.' });
-          }
-        }
-      }*/
-    //});
-
+  clickBtn(event: ClickEvent): void {
+    const button = event.target.id;
+    const step = this.GTService.getstepNum();
+    if (button === 'Back' && step > 0)
+    {
+      this.GTService.decrementStepNum();
+      this.GTService.tutorialManager();
     }
-
-    this.snackbarService.add({ message: 'Click the indicated marker and track to make your move' });
-    
+    // how many steps we have
+    // variable depending on who goes first???
+    else if (button === 'Next' && step < 5){
+      this.GTService.incrementStepNum();
+      this.GTService.tutorialManager();
+    }
   }
-
-
-
+        
 }
--- src/app/pages/game/services/guided-tutorial/guided-tutorial.service.ts
+++ src/app/pages/game/services/guided-tutorial/guided-tutorial.service.ts
@@ -12,7 +12,10 @@ import { LocalStorageService } from '../../../../shared/services/local-storage/l
 })
 export class GuidedTutorialService {
 
-  public humanPlayer: string;
+  private humanPlayer: string;
+  private stepNum: number;
+  private moveNum: number;
+  private freezeNext: boolean;
 
   constructor(
     public readonly gameManager: ManagerService,
@@ -20,9 +23,139 @@ export class GuidedTutorialService {
     private readonly snackbarService: SnackbarService
   ) { 
     this.humanPlayer = this.storageService.fetch('firstplayer');
+    this.stepNum = 0;
+    this.moveNum = 1;
+    this.freezeNext = false;
+    this.gameManager.createBoard(false, "R2,Y2,B3,G3,G2,00,G1,Y3,R1,B2,Y1,R3,B1");
+  }
+
+  getstepNum():number {
+    return this.stepNum;
+  }
+  incrementStepNum():void {
+    this.stepNum++;
+  }
+  decrementStepNum():void {
+    this.stepNum--;
+  }
+
+  getMoveNum():number {
+    return this.moveNum;
+  }
+  incrementMoveNum():void {
+    this.moveNum++;
   }
 
   startTutorial():void {
-    this.snackbarService.add({message: 'this is a test'});
+    this.messageOne();
+  }
+
+  tutorialManager():void{
+    switch (this.stepNum)
+    {
+      case 1:
+        this.messageOne();
+        break;
+      case 2:
+        this.messageTwo();
+        break;
+      case 3:
+        this.messageThree();
+        break;
+      case 4:
+        this.messageFour();
+        break;
+      case 5: 
+        this.messageFive();
+        break;
+      default:
+        console.log('something went wrong, tutorialManager switch');
+    }
+  }
+
+  moveManager(piece: string):void {
+    const m = this.moveNum;
+
+    if (this.humanPlayer === 'one'){
+      if(m === 1 && piece ==='') {
+        // place node
+        // call in game manager
+        this.moveNum++;
+      }
+      else if(m === 2 && piece === ''){
+        // place branch
+        // call in game manager
+        this.moveNum++;
+      }
+      else if(m === 3 && piece === ''){
+          
+        this.moveNum++;
+      }
+      else {
+        this.snackbarService.add({ message: 'Do me a favor and only click where I\'m tellin\' you to.' });
+      }
+    }
+    else if (this.humanPlayer === 'two'){
+      // place all moves for when the human is player 2
+      if(m === 1 && piece ==='') {
+        // place node
+        // call in game manager
+        this.moveNum++;
+      }
+      else if(m === 2 && piece === ''){
+        // place branch
+        // call in game manager
+        this.moveNum++;
+      }
+      else if(m === 3 && piece === ''){
+        // end move
+        this.moveNum++;
+      }
+      else {
+        this.snackbarService.add({ message: 'Do me a favor and only click where I\'m tellin\' you to.' });
+      }
+    }
+  }
+
+  messageOne():void{
+      // message 1, start tutorial
+      this.snackbarService.add({ message: 'Welcome in to the mines!' });
+      this.snackbarService.add({ message: 'Let’s walk you through a few steps to get y’all on the right foot with this here underground duel.' });
+      this.snackbarService.add({ message: 'Click the "Next" button to start the tutorial.'});
+      // click next
+  }
+
+  messageTwo():void {
+    // message 2
+    this.snackbarService.add({ message: 'Learn some mining lingo before we get going: Pickaxes and Drills are what we call "Mining Markers"' });
+    this.snackbarService.add({ message: 'Tracks get you from place to place, and these here squares here all abouts the mine are mining sites.' });
+    this.snackbarService.add({ message: 'These sites have gems in ‘em that hold the type and number of gem you can mine from there.' });
+    // click next
+  }
+
+  messageThree():void {
+    if(this.humanPlayer === 'one') {
+      // message 3
+      this.snackbarService.add({ message: 'Start the competition by puttin\' down a pickaxe and a connectin\' track anywhere in the mine'});
+      this.snackbarService.add({ message: 'Click the indicated marker and track to make your move' });
+      this.freezeNext = true; 
+      // must click pieces before next
+    }
+
+  }
+
+  messageFour():void {
+    if(this.humanPlayer === 'one') {
+      // message 4
+     // this.snackbarService.add({ message: 'You get gems based on the corners of the minin\' sites you have your pickaxe touchin\'' });
+      //this.snackbarService.add({ message: 'You gotta have gems in the future to get more pickaxes and tracks down the road' });
+      //this.snackbarService.add({ message: 'Don\'t like where you clicked? Click the "Undo" button' });
+      //this.snackbarService.add({ message: 'Settled on your move? Click the "End Turn" button to keep the game movin\'' });
+      // moves when "End Turn" is clicked
+    }
+  }
+
+  messageFive():void {
+
   }
 }
