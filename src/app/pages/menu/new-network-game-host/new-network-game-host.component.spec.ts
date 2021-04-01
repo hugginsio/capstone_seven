118cdefebcebe456ab589d57687d8cf18d0035df
Kyle Huggins <5430818+kjhx@users.noreply.github.com>
2021-04-01 17:16:00 -0500

    tests: import missing imports

--- src/app/pages/menu/new-network-game-host/new-network-game-host.component.spec.ts
+++ src/app/pages/menu/new-network-game-host/new-network-game-host.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { NewNetworkGameHostComponent } from './new-network-game-host.component';
 
@@ -8,6 +9,7 @@ describe('NewNetworkGameHostComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [ NewNetworkGameHostComponent ]
     }).compileComponents();
   });
