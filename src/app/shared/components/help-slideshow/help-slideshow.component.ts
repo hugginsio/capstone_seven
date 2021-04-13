f2568ec72dc31e2af04755002d0389426858fdc2
Kyle Huggins <5430818+kjhx@users.noreply.github.com>
2021-04-13 07:54:59 -0500

    pr: changes

--- src/app/shared/components/help-slideshow/help-slideshow.component.ts
+++ src/app/shared/components/help-slideshow/help-slideshow.component.ts
@@ -15,7 +15,6 @@ export class HelpSlideshowComponent {
   public maxSlides = 12;
 
   @Input() slidesToShow = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-
   @Input() inGame: string;
 
   constructor(
