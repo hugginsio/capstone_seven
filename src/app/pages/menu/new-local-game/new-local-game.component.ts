f463be98f534578248d32beee901ab6e2b1c9ec9
ccohu <camerontcohu@gmail.com>
2021-04-13 19:24:43 -0500

    fixed indent

--- src/app/pages/menu/new-local-game/new-local-game.component.ts
+++ src/app/pages/menu/new-local-game/new-local-game.component.ts
@@ -125,7 +125,7 @@ export class NewLocalGameComponent {
       }
       else
       {
-      this.storageService.update('board-seed', this.boardSeed);
+        this.storageService.update('board-seed', this.boardSeed);
       }
     }
 
